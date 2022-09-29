dbe375734138fc56a4f70b7e29f538d7e2e8825b
Brandon Fergerson <bfergerson@apache.org>
2022-09-30 01:17:26 +0400

    chore: trigger add breakpoint command

--- src/main/java/spp/demo/Main.java
+++ src/main/java/spp/demo/Main.java
@@ -1,9 +1,11 @@
 package spp.demo;
 
+import spp.demo.command.AddBreakpoint;
 import spp.demo.command.TailLogs;
 
 public class Main {
 
+    private static AddBreakpoint addBreakpoint = new AddBreakpoint();
     private static TailLogs tailLogs = new TailLogs();
 
     public static void main(String[] args) throws Exception {
@@ -14,7 +16,13 @@ public class Main {
     }
 
     public static void executeDemos() {
+        triggerAddBreakpoint();
         triggerTailLogs();
+    }
+
+
+    public static void triggerAddBreakpoint() {
+        addBreakpoint.simpleBreakpoint();
     }
 
     public static void triggerTailLogs() {
