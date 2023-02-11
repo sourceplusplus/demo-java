4a66efb8bee3416250f15b44d2fe1458e0652cbf
Brandon Fergerson <bfergerson@apache.org>
2023-02-12 00:42:51 +0400

    test: mem leak

--- src/main/java/spp/demo/command/ViewTraces.java
+++ src/main/java/spp/demo/command/ViewTraces.java
@@ -4,7 +4,9 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.BlockingHttpClient;
+import io.micronaut.http.client.DefaultHttpClientConfiguration;
 import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.HttpClientConfiguration;
 
 import java.net.URL;
 
@@ -22,6 +24,12 @@ import java.net.URL;
 @Controller("/command")
 public class ViewTraces {
 
+    private final HttpClientConfiguration config = new DefaultHttpClientConfiguration();
+
+    public ViewTraces() {
+        config.setExceptionOnErrorStatus(false);
+    }
+
     /**
      * Execute the `View Traces` command with your caret anywhere between lines 31 and 38 to see the traces for
      * the endpoint below. Executing this command will open a list of traces for the given endpoint. Clicking on a
@@ -29,9 +37,9 @@ public class ViewTraces {
      */
     @Get("/view-traces")
     public HttpResponse<Void> entryEndpoint() throws Exception {
-        try (HttpClient client = HttpClient.create(new URL("http://localhost:8080"))) {
+        try (HttpClient client = HttpClient.create(new URL("http://localhost:8080"), config)) {
             try (BlockingHttpClient blocking = client.toBlocking()) {
-                //blocking.retrieve("/command/view-traces/exit");
+                blocking.retrieve("/command/view-traces/exit");
             }
         }
         return HttpResponse.ok();
