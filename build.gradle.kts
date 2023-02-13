56511b6ef30effeb6e0c7aac3b20513fc1f5efdf
Brandon Fergerson <bfergerson@apache.org>
2023-02-13 18:31:29 +0400

    build(deps): bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     javaagent("plus.sourceplus.probe:probe-jvm:0.7.7.1")
 
     //used for the tail logs command
-    implementation("org.apache.skywalking:apm-toolkit-logback-1.x:8.13.0")
+    implementation("org.apache.skywalking:apm-toolkit-logback-1.x:8.14.0")
     implementation("org.slf4j:slf4j-api:2.0.2")
     implementation("ch.qos.logback:logback-classic:1.4.1")
 
